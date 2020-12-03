fd8a2618603b1ca30e11203adffd986b6c261e45
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2020-12-03 13:47:57 -0600

    finished commit

---
+++ CS_ASP_017LPB/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CS_ASP_017LPB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Instatiate 2 Date Time variables
+
+        public DateTime myFirstDate, mySecondDate;
+
+        
+
+
+
+        // Get First Date
+
+        public void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            myFirstDate = myCalendar.SelectedDate.Value;
+            
+
+
+        }
+
+        // Get Second Date
+
+        public void mySecondCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            mySecondDate = mySecondCalendar.SelectedDate.Value;
+
+
+
+        }
+
+        // Call Black Out Method
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SetBlackOutDates(myFirstDate, mySecondDate);
+
+
+
+
+        }
+
+        private void SetBlackOutDates(DateTime myFirstDate, DateTime mySecondDate)
+        {
+
+            BlackOutCalendar.BlackoutDates.Add(new CalendarDateRange(myFirstDate, mySecondDate));
+            
+            
+            
+        }
+
+
+
+
+
+    }
+
+}
